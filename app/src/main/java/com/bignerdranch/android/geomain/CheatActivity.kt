f258729d9ba78a061696d36783007db69ac1ec63
Marina <marina8ww1@gmail.com>
2023-09-07 14:03:07 +0700

    7.09.23..

--- app/src/main/java/com/bignerdranch/android/geomain/CheatActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geomain/CheatActivity.kt
@@ -8,7 +8,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 const val EXTRA_ANSWER_SHOW="com.bignerdranch.android.geoquiz.answer_shown"
-private const val EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true"
+const val EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true"
 
 class CheatActivity : AppCompatActivity() {
 
--- app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
@@ -77,6 +77,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
